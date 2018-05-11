37def3e93c15e044110eb13ece9b55eb3aec2414
Shvidkoy Mychailo <origami690@gmail.com>
2018-05-11 15:01:16 +0300

    Test synchronization with VS Community 2017

--- ParkingApp/Program.cs
+++ ParkingApp/Program.cs
@@ -7,7 +7,7 @@ namespace ParkingApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!123");
         }
     }
 
