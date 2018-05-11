54919dab9a0c57f75840dcf3dfd467b0da423c9e
Shvidkoy Mychailo <origami690@gmail.com>
2018-05-11 13:25:43 +0300

    Добавьте файлы проекта.

---
+++ ParkingApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Settings
+    {
+        public TimeSpan TimeOut { get; set;}
+        //public Dictionary
+        public int ParkingSpace => 0;
+        public decimal Fine => 0;
+    }
+
+    class Parking
+    {
+        List<Car> ListCar;
+        List<Transaction> ListTransaction;
+        decimal Balnce => 0;
+
+    }
+
+    class Car
+    {
+        public int Id { get; set; }
+        public decimal Balance => 0;
+        public CarType Ctype { get; set; }
+    }
+
+    enum CarType
+    {
+        Passenger,Truck,Bus,Motocycle
+    }
+    class Transaction
+    {
+        public DateTime DTtransaction { get; set; }
+        public int CarId { get; set; }
+        public decimal AmountMoney => 0;
+    }
+}
